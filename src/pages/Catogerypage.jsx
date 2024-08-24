9e15c49518d62050d1ab69e25d146dd68159965b
TaherTadpatri <tahertadpatri@gmail.com>
2024-08-24 12:50:14 +0530

    minor change url

--- src/pages/Catogerypage.jsx
+++ src/pages/Catogerypage.jsx
@@ -17,7 +17,7 @@ function Catogerypage() {
       setloading(true);
       try {
         const resonse = await fetch(
-          "https://storebackend-production-9a2b.up.railway.app/apiv2/catproducts/",
+          "https://frameyourmemories.up.railway.app/apiv2/catproducts/",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
